58fa000f621b75e0069540d9784c43a271d68647
pds <dasungwkd@naver.com>
2019-08-21 23:22:21 +0900

    Updates

---
+++ precache-manifest.2543ea4cd903f0474f5eaa950a8ebde9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dc0276936ecc4e62a947ec7b54439e",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "d56152be1016fc679525",
+    "url": "/movie_app_2019/static/css/main.4330eeea.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "d56152be1016fc679525",
+    "url": "/movie_app_2019/static/js/main.c1eeb849.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
