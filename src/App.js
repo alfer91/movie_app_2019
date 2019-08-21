0e2721937a56a2b2946b54d1fa4700dcd5258988
pds <dasungwkd@naver.com>
2019-08-21 18:54:05 +0900

    make html, css, class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -18,18 +19,25 @@ class App extends React.Component {
     const { isLoading, movies } = this.state;
 
     return (
-      <div>{isLoading ?
-        "Loading..."
-        : movies.map(movie => (
-          <Movie
-            key={movie.id}
-            id={movie.id}
-            year={movie.year}
-            title={movie.title}
-            summary={movie.summary}
-            poster={movie.medium_cover_image}></Movie>
-        ))}
-      </div>
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}></Movie>
+              ))}
+            </div>
+          )}
+      </section>
     );
   }
 }
