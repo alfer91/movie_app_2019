6a3295cf560800dd6cd7a9265526bd2a3403d278
pds <dasungwkd@naver.com>
2019-08-21 23:03:46 +0900

    const slice... and style css

--- src/Movie.js
+++ src/Movie.js
@@ -7,12 +7,12 @@ function Movie({ year, title, summary, poster, genres }) {
         <div className="movie">
             <img src={poster} alt={title} title={title}></img>
             <div className="movie__data">
-                <h3 className="movie__title" style={{backgroundColor:"red"}}>{title}</h3>
+                <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">{year}</h5>
-                <ul className="genres">{genres.map((genre, index) => (
+                <ul className="movie__genres">{genres.map((genre, index) => (
                     <li key={index} className="genres_genre">{genre}</li>
                 ))}</ul>
-                <p className="movie__summary">{summary}</p>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
             </div>
         </div>
     );
