921412673e4f4e2f5373dfbcf1bee7ba8bfb6c99
pds <dasungwkd@naver.com>
2019-08-21 16:58:12 +0900

    Loading movie api use axios intead of fetch. then async, await

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component {
-// the component life cycle : mounting (constructor, render, componentDidMount) -> update (componentDidUpdate) -> unmounting (componentWillUnmount)
-
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-  
   state = {
-    count:0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({count:current.count+1}));
-  };
-  minus = () => {
-    this.setState(current => ({count:current.count-1}));
-  };
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies.data.data.movies);
+  }
   componentDidMount(){
-    console.log("component redered");
-  }
-  componentDidUpdate(){
-    console.log("updated");
-  }
-  componentWillUnmount(){
-    console.log("unmount")
+    this.getMovies();
   }
   render(){
-    console.log("rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
+    const {isLoading} = this.state;
+     return (
+      <div>{isLoading ? "Loading..." : "We are ready"}</div>
     );
   }
 }
